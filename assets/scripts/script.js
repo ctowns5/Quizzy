2712cb3a4c8e532badf305eb100e48fea6ecafa2
ctowns5 <128754073+ctowns5@users.noreply.github.com>
2023-05-25 02:31:15 -0600

    added initial html, css & js

---
+++ assets/scripts/script.js
@@ -0,0 +1,85 @@
+//high scores stored in Array, displayed after game
+//questions
+//1.commonly used data types do NOT include , answer booleans, other choices strings, alerts, numbers
+//2.
+var quizData = [{
+    question: "A very useful tool used during development and debugging is?",
+    a: "terminal/bash",
+    b: "for loops",
+    c: "console.log",
+    d: "javascript",
+    correct: "c",
+    },
+    {
+    question:
+    "String values must be enclosed within_____ when being assigned to variables?",
+    a: "commas",
+    b: "curley brackets",
+    c: "quotes",
+    d: "parenthesis",
+    correct: "b",
+    },
+    {
+    question: "Arrays in JavaScript can be used to store_____?",
+    a: "numbers and strings",
+    b: "other arrays",
+    c: "booleans",
+    d: "all of the above",
+    correct: "d",
+    },
+    {
+    question: "Commonly used data types do NOT include ____?",
+    a: "strings",
+    b: "booleans",
+    c: "alerts",
+    d: "numbers",
+    correct: "b",
+    },
+];
+var quiz = document.getElementById("quiz");
+var answerEls = document.querySelectorAll(".answer");
+var questionEl = document.getElementById("question");
+var a_text = document.getElementById("a_text");
+var b_text = document.getElementById("b_text");
+var c_text = document.getElementById("c_text");
+var d_text = document.getElementById("d_text");
+var submitBtn = document.getElementById("submit");
+let currentQuiz = 0;
+let score = 0;
+loadQuiz();
+function loadQuiz() {
+    deselectAnswers();
+    var currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+function getSelected() {
+    let answer;
+    answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+    answer = answerEl.id;
+    }
+    });
+    return answer;
+}
+submitBtn.addEventListener("click", () => {
+    var answer = getSelected();
+    if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+    score++;
+}
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+    loadQuiz();
+    } else {
+    quiz.innerHTML = `
+        <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+        <button onclick="location.reload()">Restart Quiz</button>
+        `;
+}}});
