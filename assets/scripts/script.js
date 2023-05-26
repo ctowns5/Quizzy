9a12fc8f3be5f87df78fc2a4dd38899d5873a518
ctowns5 <128754073+ctowns5@users.noreply.github.com>
2023-05-25 18:57:29 -0600

    got timer working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,42 +1,39 @@
-//high scores stored in Array, displayed after game
-//questions
-//1.commonly used data types do NOT include , answer booleans, other choices strings, alerts, numbers
-//2.
-var quizData = [{
+var quizData = [
+  {
     question: "A very useful tool used during development and debugging is?",
     a: "terminal/bash",
     b: "for loops",
     c: "console.log",
     d: "javascript",
     correct: "c",
-    },
-    {
+  },
+  {
     question:
-    "String values must be enclosed within_____ when being assigned to variables?",
+      "String values must be enclosed within_____ when being assigned to variables?",
     a: "commas",
     b: "curley brackets",
     c: "quotes",
     d: "parenthesis",
     correct: "b",
-    },
-    {
+  },
+  {
     question: "Arrays in JavaScript can be used to store_____?",
     a: "numbers and strings",
     b: "other arrays",
     c: "booleans",
     d: "all of the above",
     correct: "d",
-    },
-    {
+  },
+  {
     question: "Commonly used data types do NOT include ____?",
     a: "strings",
     b: "booleans",
     c: "alerts",
     d: "numbers",
     correct: "b",
-    },
+  },
 ];
-var timerEl =document.getElementById("timer");
+var timerEl = document.getElementById("timer");
 var quiz = document.getElementById("quiz");
 var answerEls = document.querySelectorAll(".answer");
 var questionEl = document.getElementById("question");
@@ -50,42 +47,60 @@ let score = 75;
 loadQuiz();
 
 function loadQuiz() {
-    timerEl.textContent = score;
-    deselectAnswers();
-    var currentQuizData = quizData[currentQuiz];
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
+  timerEl.textContent = score;
+  deselectAnswers();
+  var currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
 }
 function deselectAnswers() {
-    answerEls.forEach((answerEl) => (answerEl.checked = false));
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
 function getSelected() {
-    let answer;
-    answerEls.forEach((answerEl) => {
+  let answer;
+  answerEls.forEach((answerEl) => {
     if (answerEl.checked) {
-    answer = answerEl.id;
+      answer = answerEl.id;
     }
-    });
-    return answer;
+  });
+  return answer;
 }
+function countdown() {
+  var timeLeft = 75;
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      timerEl.textContent = "";
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+countdown();
 submitBtn.addEventListener("click", () => {
-    var answer = getSelected();
-    if (answer) {
+  var answer = getSelected();
+  if (answer) {
     if (answer === quizData[currentQuiz].correct) {
-    // score++;
-}
-    else {
-        score-=10
+      // score++;
+    } else {
+      score -= 10;
     }
     currentQuiz++;
     if (currentQuiz < quizData.length) {
-    loadQuiz();
+      loadQuiz();
     } else {
-    quiz.innerHTML = `
+      quiz.innerHTML = `
         <h2>You scored ${score}</h2>
         <button onclick="location.reload()">Restart Quiz</button>
         `;
-}}});
+    }
+  }
+});
